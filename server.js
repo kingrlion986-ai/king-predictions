d5a0bf1dfb20c1b4943da23f659f8e2fd431aa3e
kingrlion986-ai <kingrlion986@gmail.com>
2026-07-05 11:16:36 +0100

    Refactor static file serving in server.js
    Updated static file serving to use path.join for better compatibility.

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ const {
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.get("/vip/matches", async (req, res) => {
   try {
