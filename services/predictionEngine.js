4fd6ba8da0c3ede35d926081fde28dfb2dcb8639
kingrlion986-ai <kingrlion986@gmail.com>
2026-07-20 14:16:54 +0100

    Optimize team analysis with Promise.all

--- services/predictionEngine.js
+++ services/predictionEngine.js
@@ -68,11 +68,10 @@ console.time(timer);
         return cached.data;
     }
 
-    const homeStats =
-    await analyzeTeam(match.homeTeam);
-
-const awayStats =
-    await analyzeTeam(match.awayTeam);
+    const [homeStats, awayStats] = await Promise.all([
+    analyzeTeam(match.homeTeam),
+    analyzeTeam(match.awayTeam)
+]);
     const homeElo =
         getTeamElo(match.homeTeam.id);
 
