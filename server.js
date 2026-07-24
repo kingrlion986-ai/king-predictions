eb16eea75a9455702313ddcb6348a27b9a328210
kingrlion986-ai <kingrlion986@gmail.com>
2026-07-24 18:30:50 +0100

    Remove express app initialization from server.js
    Removed initialization of the express app.

--- server.js
+++ server.js
@@ -38,8 +38,6 @@ const app = express();
 // Chargement des bases IA
 initializeDatabase();
 
-const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
